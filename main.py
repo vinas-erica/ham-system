a736163ed02593d9f61e9f19ff0d4c01adef24fb
Melvin Perez <perezmelvin74@gmail.com>
2022-07-01 00:31:23 +0800

    reading data with json but it still in comment

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def index(request: Request) -> templates.TemplateResponse:
 # async def index(request: Request):
     
 #     return templates.TemplateResponse("index.html", request)
-async def generate_random_data(request: Request) -> Iterator[str]:
+async def humidityData(request: Request) -> Iterator[str]:
     """
     Generates random value between 0 and 100
 
@@ -45,45 +45,153 @@ async def generate_random_data(request: Request) -> Iterator[str]:
         yield f"data:{json_data}\n\n"
         await asyncio.sleep(1)
 
+async def lightData(request: Request) -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    client_ip = request.client.host
+
+    logger.info("Client %s connected", client_ip)
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%H:%M:%S"),
+                "value": random.random() * 100,
+            }
+        )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
+
+async def phData(request: Request) -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    client_ip = request.client.host
+
+    logger.info("Client %s connected", client_ip)
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%H:%M:%S"),
+                "value": random.random() * 100, #getting data, this will be change
+            }
+        )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
+
+async def tempData(request: Request) -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    client_ip = request.client.host
+
+    logger.info("Client %s connected", client_ip)
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%H:%M:%S"),
+                "value": random.random() * 100, #getting data, this will be change
+                # with open('canada.json','r') as f: #opening the file
+                #         country_object = json.load(f)
+            }
+        )
+    # with open('file.json', 'r') as f:
+    #     data = json.load(f)
+    #     json_data = json.dumps(
+    #         "time": datetime.now().strftime("%H:%M:%S"),
+    #         "value": data,
+    #     )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
+
+async def waterLevelData(request: Request) -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    client_ip = request.client.host
+
+    logger.info("Client %s connected", client_ip)
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%H:%M:%S"),
+                "value": random.random() * 100, #getting data, this will be change
+            }
+        )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
+
+async def waterTempData(request: Request) -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    client_ip = request.client.host
+
+    logger.info("Client %s connected", client_ip)
+
+    while True:
+        json_data = json.dumps(
+            {
+                "time": datetime.now().strftime("%H:%M:%S"),
+                "value": random.random() * 100, #getting data, this will be change
+            }
+        )
+        yield f"data:{json_data}\n\n"
+        await asyncio.sleep(1)
+
 
 @app.get("/chart-data-water-temp", response_class=HTMLResponse)
 async def chart_data(request: Request) -> StreamingResponse:
-    responses = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    responses = StreamingResponse(waterTempData(request), media_type="text/event-stream")
     responses.headers["Cache-Control"] = "no-cache"
     responses.headers["X-Accel-Buffering"] = "no"
     return responses
 
 @app.get("/chart-data-water-level", response_class=HTMLResponse)
 async def chart_data(request: Request) -> StreamingResponse:
-    responses = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    responses = StreamingResponse(waterLevelData(request), media_type="text/event-stream")
     responses.headers["Cache-Control"] = "no-cache"
     responses.headers["X-Accel-Buffering"] = "no"
     return responses
 
 @app.get("/chart-data-temp", response_class=HTMLResponse)
 async def chart_data(request: Request) -> StreamingResponse:
-    responses = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    responses = StreamingResponse(tempData(request), media_type="text/event-stream")
     responses.headers["Cache-Control"] = "no-cache"
     responses.headers["X-Accel-Buffering"] = "no"
     return responses
 
 @app.get("/chart-data-ph", response_class=HTMLResponse)
 async def chart_data(request: Request) -> StreamingResponse:
-    responses = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    responses = StreamingResponse(phData(request), media_type="text/event-stream")
     responses.headers["Cache-Control"] = "no-cache"
     responses.headers["X-Accel-Buffering"] = "no"
     return responses
 
 @app.get("/chart-data-light", response_class=HTMLResponse)
 async def chart_data(request: Request) -> StreamingResponse:
-    responses = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    responses = StreamingResponse(lightData(request), media_type="text/event-stream")
     responses.headers["Cache-Control"] = "no-cache"
     responses.headers["X-Accel-Buffering"] = "no"
     return responses
 
 @app.get("/chart-data-humidity", response_class=HTMLResponse)
 async def chart_data(request: Request) -> StreamingResponse:
-    responses = StreamingResponse(generate_random_data(request), media_type="text/event-stream")
+    responses = StreamingResponse(humidityData(request), media_type="text/event-stream")
     responses.headers["Cache-Control"] = "no-cache"
     responses.headers["X-Accel-Buffering"] = "no"
     return responses
